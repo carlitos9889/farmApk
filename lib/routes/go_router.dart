1d1dd3a6ead9394a7bef974adc59e18dacbb8d4e
carlitos9889 <89477458+carlitos9889@users.noreply.github.com>
2022-11-20 02:17:26 -0500

    no funciona la animacion

--- lib/routes/go_router.dart
+++ lib/routes/go_router.dart
@@ -5,6 +5,7 @@ import 'package:farmapk/pages/pdf_view_page.dart';
 import 'package:farmapk/pages/quiz_for_theme.dart';
 import 'package:farmapk/pages/theme_info_page.dart';
 import 'package:farmapk/pages/theme_page.dart';
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 GoRouter getGoRouterApp() => GoRouter(
@@ -18,15 +19,30 @@ GoRouter getGoRouterApp() => GoRouter(
             GoRoute(
               path: 'orientaciones',
               builder: (_, state) => const OrientacionesGeneralesPage(),
+              pageBuilder: (context, state) => buildTransition<void>(
+                context: context,
+                state: state,
+                child: const OrientacionesGeneralesPage(),
+              ),
             ),
             GoRoute(
               path: 'pdf',
               builder: (_, state) => PdfViewPage(state.extra as String),
+              pageBuilder: (context, state) => buildTransition<void>(
+                context: context,
+                state: state,
+                child: PdfViewPage(state.extra as String),
+              ),
             ),
             GoRoute(
               path: 'guias',
               builder: (_, state) =>
                   ThemePage(tema: state.extra as List<TemaGuia>),
+              pageBuilder: (context, state) => buildTransition<void>(
+                context: context,
+                state: state,
+                child: ThemePage(tema: state.extra as List<TemaGuia>),
+              ),
               routes: [
                 GoRoute(
                   path: ':index',
@@ -35,6 +51,14 @@ GoRouter getGoRouterApp() => GoRouter(
                     final temas = state.extra as List<TemaGuia>;
                     return ThemeInfoPage(temas: temas, index: index);
                   },
+                  pageBuilder: (context, state) => buildTransition(
+                    context: context,
+                    state: state,
+                    child: ThemeInfoPage(
+                      temas: state.extra as List<TemaGuia>,
+                      index: int.parse(state.params['index']!),
+                    ),
+                  ),
                 ),
               ],
             ),
@@ -43,8 +67,26 @@ GoRouter getGoRouterApp() => GoRouter(
               builder: (context, state) => QuizForThemePage(
                 dataTema: state.extra as DataTema,
               ),
+              pageBuilder: (context, state) => buildTransition(
+                context: context,
+                state: state,
+                child: QuizForThemePage(dataTema: state.extra as DataTema),
+              ),
             ),
           ],
         ),
       ],
     );
+
+CustomTransitionPage buildTransition<T>({
+  required BuildContext context,
+  required GoRouterState state,
+  required Widget child,
+}) {
+  return CustomTransitionPage<T>(
+    key: state.pageKey,
+    child: child,
+    transitionsBuilder: (context, animation, secondaryAnimation, child) =>
+        FadeTransition(opacity: animation, child: child),
+  );
+}
